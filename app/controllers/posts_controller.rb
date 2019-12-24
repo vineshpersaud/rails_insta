4bacaae92d88a8dfd77468505f31d33211ab6f2b
github username <vineshpersaud@gmail.com>
2019-12-23 23:59:59 -0500

    basic image upload and view working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
-  def create
+  
+  def new
+    @post = Post.new
   end
 
-  def view
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    end
+  end
+
+  def show
+    @post = Post.find(params['id'])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title,:image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts/create'
-  get 'posts/view'
+  post 'posts/create'
+  get 'posts/new'
+  get 'posts/show'
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
