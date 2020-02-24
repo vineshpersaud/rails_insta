f25a99a0bd3e06a4a5250c88ef63b29d90ca57a3
github username <vineshpersaud@gmail.com>
2020-02-24 16:55:35 -0500

    basic navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+    
     def current_user
         return unless session[:user_id]
         @current_user ||= User.find(session[:user_id])
